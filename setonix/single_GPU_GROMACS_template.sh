04933cd181857aa3d09a7a1cff02027d974e15f7
Sharif Nada <s.nada@uq.edu.au>
2023-09-27 15:15:26 +1000

    Update single_GPU_GROMACS_template.sh
    removed old slurm directives

--- setonix/single_GPU_GROMACS_template.sh
+++ setonix/single_GPU_GROMACS_template.sh
@@ -2,10 +2,7 @@
 #SBATCH --job-name=<JOB_NAME>
 #SBATCH --partition=gpu
 #SBATCH --nodes=1              #1 nodes in this example
-#SBATCH --ntasks-per-node=1    #1 tasks for the 1 GPUs in this job
 #SBATCH --gpus-per-node=1      #1 GPUs in this job
-#SBATCH --sockets-per-node=1   #Use the 1 slurm-sockets in this job
-#SBATCH --cpus-per-task=8
 #SBATCH --time=01:00:00
 #SBATCH --account=<ACCOUNT>-gpu
 
