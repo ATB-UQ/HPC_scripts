1181f8e2275f8dc6dbae98f18de2aec0235e84ff
Sharif Nada <s.nada@uq.edu.au>
2023-10-06 10:47:38 +1000

    Update single_GPU_GROMACS_template.sh
    added --gpu-bind=closest srun directive

--- setonix/single_GPU_GROMACS_template.sh
+++ setonix/single_GPU_GROMACS_template.sh
@@ -11,4 +11,4 @@ module load gromacs-amd-gfx90a/2023
 export OMP_NUM_THREADS=8
 export GMX_MAXBACKUP=-1
 
-srun -l -u -N 1 -n 1 -c 8 --gpus-per-node=1 --gpus-per-task=1 gmx_mpi mdrun <ARGS> -nb gpu -bonded gpu
+srun -l -u -N 1 -n 1 -c 8 --gpus-per-node=1 --gpus-per-task=1 --gpu-bind=closest gmx_mpi mdrun <ARGS> -nb gpu -bonded gpu
